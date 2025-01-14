8f07c4528bcb70ed27138bd31c3cefa8ac2afde1
kiuuon <kiuuon29@gmail.com>
2025-01-14 13:41:48 +0900

    chore: prettier 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Metro",
+  title: 'Metro',
 };
 
 export default function RootLayout({
