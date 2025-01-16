7dc5638b5e491ced1a838907450fbc14eb23555a
Kiwon Kim <kiuuon29@gmail.com>
2025-01-16 11:57:16 +0900

    feat: 로그인 페이지 UI (#6)
    * feat: 로그인 페이지 UI 구현
    
    * feat: 로그인 버튼 UI 구현
    
    * feat: font 적용 방법 변경
    
    * style: css 이름 수정 및 1rem로 설정된 gap  base로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Noto_Sans_KR } from 'next/font/google';
 import type { Metadata } from 'next';
+import LoginPage from './_components/login/login-page';
 
 export const metadata: Metadata = {
   title: 'Metro',
@@ -15,6 +16,18 @@ const RootLayout = ({
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const isLogin = false;
+
+  if (!isLogin) {
+    return (
+      <html lang="ko">
+        <body className={notoSans.className}>
+          <LoginPage />
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="ko">
       <body className={notoSans.className}>{children}</body>
