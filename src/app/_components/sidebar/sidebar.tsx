3119df7e4731dc33c5f9a7ebe943d7db8fca750e
kiuuon <kiuuon29@gmail.com>
2025-01-16 14:47:13 +0900

    chore: sidebar 폴더 구조 변경

--- src/app/_components/sidebar/sidebar.tsx
+++ src/app/_components/sidebar/sidebar.tsx
@@ -1,13 +1,14 @@
 import { css } from '@/../styled-system/css';
 
-import ProfileCard from './profile-card';
-import SideMenuCard from './side-menu-card';
-import FinderCard from './finder-card';
+import ProfileCard from './profile/profile-card';
+import SideMenuCard from './side-menu/side-menu-card';
+import FinderCard from './finder/finder-card';
 
 const sideBar = css({
   width: '18rem',
   height: '100vh',
   display: 'flex',
+  flex: '1',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'flex-start',
