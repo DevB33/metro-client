d1db81e8aa7eef7a7c4f07e0a78febe4e2e39c32
이동율 <dy9623@naver.com>
2025-01-17 15:12:25 +0900

    feat: 새 페이지 생성 구현

--- src/app/_components/sidebar/finder/finder-card.tsx
+++ src/app/_components/sidebar/finder/finder-card.tsx
@@ -2,6 +2,7 @@ import { css } from '@/../styled-system/css';
 
 import { useEffect, useState } from 'react';
 import IPageType from '@/types/page-type';
+import PlusIcon from '@/icons/plus-icon';
 import PageItem from './page-item';
 import pageList from './page-list-mock';
 
@@ -18,8 +19,43 @@ const finderCard = css({
   boxShadow: 'sidebar',
 });
 
+const pageItem = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
+  height: '2rem',
+  px: 'tiny',
+  gap: '0.25rem',
+  fontWeight: 'regular',
+  fontSize: 'md',
+  borderRadius: '0.25rem',
+  cursor: 'pointer',
+
+  _hover: {
+    backgroundColor: '#F1F1F0',
+  },
+});
+
+const pageButtonContainer = css({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+});
+
+const pageButton = css({
+  borderRadius: '0.25rem',
+  cursor: 'pointer',
+
+  _hover: {
+    backgroundColor: '#E4E4E3',
+  },
+});
+
 const FinderCard = () => {
   const [mockPageList, setMockPageList] = useState<IPageType[]>();
+  const [isHover, setIsHover] = useState(false);
 
   useEffect(() => {
     const storedPageList = localStorage.getItem('pageList');
@@ -31,8 +67,36 @@ const FinderCard = () => {
     }
   }, []);
 
+  const createPage = () => {
+    const newPage: IPageType = {
+      pageId: Date.now(), // 고유한 ID 생성
+      title: '새 페이지',
+      icon: '', // 기본 아이콘
+      children: [], // 기본적으로 빈 children 배열
+    };
+    const updatedPageList = [...(mockPageList || []), newPage];
+
+    // 로컬 스토리지와 상태 업데이트
+    localStorage.setItem('pageList', JSON.stringify(updatedPageList));
+    setMockPageList(updatedPageList);
+  };
+
   return (
     <div className={finderCard}>
+      <div
+        className={pageItem}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        기원 님의 workspace
+        {isHover && (
+          <div className={pageButtonContainer}>
+            <button type="button" className={pageButton} onClick={createPage}>
+              <PlusIcon />
+            </button>
+          </div>
+        )}
+      </div>
       {mockPageList?.map(page => {
         return <PageItem key={page.pageId} page={page} depth={1} />;
       })}
