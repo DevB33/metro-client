6e2ec1a8de5716d96e16956a70a64115a04e4a81
ljs831 <ljsljs2580@gmail.com>
2025-01-21 03:38:43 +0900

    feat: 검색 키워드 지우기 및 결과 리스트 출력

--- src/app/@modal/(.)search-modal/_component/searchbar.tsx
+++ src/app/@modal/(.)search-modal/_component/searchbar.tsx
@@ -1,11 +1,13 @@
 'useclient';
 
 import { css } from '@/../styled-system/css';
+import { useState } from 'react';
 
 import SearchIcon from '@/icons/search-icon';
+import CancleIcon from '@/icons/cancle-icon';
 
 const searchBar = css({
-  height: '4rem',
+  height: '3rem',
   width: '100%',
   display: 'flex',
   justifyContent: 'flex-start',
@@ -16,6 +18,7 @@ const searchBar = css({
   fontSize: 'md',
   cursor: 'text',
   backgroundColor: { base: '#F1F1F0', _hover: 'lightgray' },
+  transition: '0.2s',
 });
 
 const textInput = css({
@@ -25,14 +28,40 @@ const textInput = css({
   fontSize: 'md',
   backgroundColor: 'transparent',
   border: 'none',
+  outline: 'none',
   color: 'black',
 });
 
+const cancleButton = css({
+  display: 'flex',
+  alignItems: 'center',
+  gap: 'tiny',
+  cursor: 'pointer',
+});
+
 const SearchBar = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const clearInput = () => {
+    setInputValue('');
+  };
   return (
     <div className={searchBar}>
       <SearchIcon color="black" />
-      <input className={textInput} type="text" placeholder="Search Page or Keyword" />
+      <input
+        className={textInput}
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        placeholder="Search Page or Keyword"
+      />
+      <div className={cancleButton} onClick={clearInput}>
+        <CancleIcon color="black" />
+      </div>
     </div>
   );
 };
--- src/app/@modal/(.)search-modal/page.tsx
+++ src/app/@modal/(.)search-modal/page.tsx
@@ -4,6 +4,8 @@ import { css } from '@/../styled-system/css';
 import { useRouter } from 'next/navigation';
 
 import SearchBar from './_component/searchbar';
+import SearchResult from './_component/searchResult';
+import searchResultList from './search-list-mock';
 
 const overlayStyles = css({
   position: 'fixed',
@@ -24,18 +26,56 @@ const searchModalContainer = css({
   transform: 'translate(-50%, -50%)',
   borderRadius: '1rem',
   display: 'flex',
-  flexDirection: 'row',
+  flexDirection: 'column',
   boxShadow: 'settingModal',
   backgroundColor: 'white',
   overflow: 'hidden',
   zIndex: 1000,
 });
 
+const searchResultContainer = css({
+  display: 'flex',
+  flex: '1',
+  flexDirection: 'column',
+  pt: '0.5rem',
+  px: '1rem',
+  overflow: 'hidden',
+});
+
+const searchListContainer = css({
+  flex: '1',
+  overflow: 'auto',
+  py: '0.3rem',
+});
+
+const resultTitle = css({
+  pl: 'tiny',
+  fontSize: '1rem',
+  height: '2rem',
+  fontWeight: 'bold',
+  color: 'gray',
+});
+
+const noResult = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  fontSize: 'md',
+  color: 'gray',
+});
+
+const divider = css({
+  backgroundColor: 'lightgray',
+  height: '1px',
+  borderRadius: '1rem',
+});
+
 const SearchModal = () => {
   const router = useRouter();
 
   const closeModal = () => {
-    router.back(); // 이전 상태로 돌아감
+    router.back();
   };
 
   return (
@@ -43,6 +83,17 @@ const SearchModal = () => {
       <div className={overlayStyles} onClick={closeModal} />
       <div className={searchModalContainer}>
         <SearchBar />
+        <div className={searchResultContainer}>
+          <div className={resultTitle}>검색 결과</div>
+          <div className={divider}></div>
+          <div className={searchListContainer}>
+            {searchResultList.length > 0 ? (
+              searchResultList.map(page => <SearchResult key={page.pageId} page={page} />)
+            ) : (
+              <div className={noResult}>결과가 없습니다.</div>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
