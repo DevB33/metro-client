4e09a557e434e94cdf66df305322069b3adcad46
이동율 <dy9623@naver.com>
2025-01-20 16:05:03 +0900

    feat: placeholder 대신 포커스 됐을 때 문구 띄우도록 변경

--- src/app/note/[id]/_components/_content/note-content.tsx
+++ src/app/note/[id]/_components/_content/note-content.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { css } from '@/../styled-system/css';
 
 const noteContentContainer = css({
@@ -18,22 +18,49 @@ const noteContentContainer = css({
   _placeholder: { color: 'gray' },
 });
 
+const focusTextStyle = css({
+  position: 'absolute',
+  color: 'gray',
+  fontSize: 'md',
+  top: 'tiny',
+  pointerEvents: 'none',
+});
+
+const wrapper = css({
+  position: 'relative',
+});
+
 const NoteContent = () => {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [hasValue, setHasValue] = useState(false);
 
   const handleInput = () => {
     if (textAreaRef.current) {
       textAreaRef.current.style.height = 'auto';
       textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+      setHasValue(textAreaRef.current.value.length > 0);
     }
   };
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
-    <textarea
-      ref={textAreaRef}
-      className={noteContentContainer}
-      onInput={handleInput}
-      placeholder="글을 작성하거나 AI를 사용하려면 '스페이스' 키를, 명령어를 사용하려면 '/' 키를 누르세요."
-    />
+    <div className={wrapper}>
+      {isFocused && !hasValue && (
+        <div className={focusTextStyle}>
+          글을 작성하거나 AI를 사용하려면 '스페이스' 키를, 명령어를 사용하려면 '/' 키를 누르세요.
+        </div>
+      )}
+      <textarea
+        ref={textAreaRef}
+        className={noteContentContainer}
+        onInput={handleInput}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      />
+    </div>
   );
 };
 
