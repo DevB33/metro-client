29bb4db501ef09a62d9a0a3e534f601185807bac
ljs831 <ljsljs2580@gmail.com>
2025-02-19 15:37:35 +0900

    feat: title 여러줄 입력시 일부 스크롤로 보이도록 처리

--- src/app/(main)/note/[id]/_components/note-header/title.tsx
+++ src/app/(main)/note/[id]/_components/note-header/title.tsx
@@ -1,19 +1,61 @@
+import { useState, useRef } from 'react';
 import { css } from '@/../styled-system/css';
 
 const titleFont = css({
   fontSize: 'lg',
   fontWeight: 'bold',
-  _placeholder: {
+  width: '100%',
+  wordBreak: 'break-all',
+  resize: 'none',
+  overflowY: 'auto',
+  minHeight: '48px',
+  maxHeight: '110px',
+  lineHeight: '1.5',
+  padding: '8px',
+  border: 'none',
+  outline: 'none',
+  '&::placeholder': {
     color: 'lightgray',
   },
-  _focus: {
-    outline: 'none',
-  },
-  width: '100%',
 });
 
 const Title = () => {
-  return <input className={titleFont} placeholder="새 페이지" />;
+  const [value, setValue] = useState('');
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+    const textarea = textareaRef.current;
+
+    if (textarea) {
+      textarea.style.height = `${Math.min(textarea.scrollHeight, 110)}px`;
+    }
+  };
+
+  const handleMouseLeave = () => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <textarea
+      ref={textareaRef}
+      className={titleFont}
+      placeholder="새 페이지"
+      rows={1}
+      value={value}
+      onChange={handleChange}
+      onMouseLeave={handleMouseLeave}
+      onKeyDown={handleKeyDown}
+    />
+  );
 };
 
 export default Title;
