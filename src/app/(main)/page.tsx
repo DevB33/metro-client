92e61fe45fc498a1d3318429ce466bbf64cb7f7c
이동율 <dy9623@naver.com>
2025-04-07 21:16:43 +0900

    feat : home 페이지 기본 틀 구현

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,30 @@
+import { css } from '@/../styled-system/css';
+
+const homePage = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100vw',
+  height: '100vh',
+  gap: 'base',
+});
+
+const welcomeBanner = css({
+  fontSize: 'lg',
+});
+
+const visualContainer = css({
+  width: '80%',
+  height: '60%',
+  backgroundColor: 'grey',
+});
+
 const Home = () => {
   return (
-    <div>
-      <div>메인 페이지</div>
+    <div className={homePage}>
+      <div className={welcomeBanner}>김기원 님, 안녕하세요.</div>
+      <div className={visualContainer}>sdf</div>
     </div>
   );
 };
