54f35e9dedbe625578e07e51f35d98cdb033dfe1
이동율 <dy9623@naver.com>
2025-04-10 21:56:11 +0900

    feat : 색상 reset 버튼 구현

--- src/app/(main)/_components/chart/LinkControls.tsx
+++ src/app/(main)/_components/chart/LinkControls.tsx
@@ -1,61 +1,51 @@
 import React from 'react';
+import { css } from '@/../styled-system/css';
+import ResetIcon from '@/icons/reset-icon';
 
-const controlStyles = { fontSize: 10 };
+const controlStyles = css({ fontSize: 10, display: 'flex', flexDirection: 'row', justifyContent: 'space-between' });
 
 interface Props {
   layout: string;
-  orientation: string;
   linkType: string;
   stepPercent: number;
   setLayout: (layout: string) => void;
-  setOrientation: (orientation: string) => void;
   setStepPercent: (percent: number) => void;
+  resetColor?: () => void;
 }
 
-const LinkControls = ({
-  layout,
-  orientation,
-  linkType,
-  stepPercent,
-  setLayout,
-  setOrientation,
-  setStepPercent,
-}: Props) => {
+const resetButton = css({
+  mx: '1rem',
+  my: '0.5rem',
+});
+
+const LinkControls = ({ layout, linkType, stepPercent, setLayout, setStepPercent, resetColor }: Props) => {
   return (
-    <div style={controlStyles}>
-      <label>layout:</label>&nbsp;
-      <select onClick={e => e.stopPropagation()} onChange={e => setLayout(e.target.value)} value={layout}>
-        <option value="cartesian">cartesian</option>
-        <option value="polar">polar</option>
-      </select>
-      &nbsp;&nbsp;
-      <label>orientation:</label>&nbsp;
-      <select
-        onClick={e => e.stopPropagation()}
-        onChange={e => setOrientation(e.target.value)}
-        value={orientation}
-        disabled={layout === 'polar'}
-      >
-        <option value="vertical">vertical</option>
-        <option value="horizontal">horizontal</option>
-      </select>
-      &nbsp;&nbsp;
-      {linkType === 'step' && layout !== 'polar' && (
-        <>
-          &nbsp;&nbsp;
-          <label>step:</label>&nbsp;
-          <input
-            onClick={e => e.stopPropagation()}
-            type="range"
-            min={0}
-            max={1}
-            step={0.1}
-            onChange={e => setStepPercent(Number(e.target.value))}
-            value={stepPercent}
-            disabled={linkType !== 'step' || layout === 'polar'}
-          />
-        </>
-      )}
+    <div className={controlStyles}>
+      <div>
+        <label>layout:</label>&nbsp;
+        <select onClick={e => e.stopPropagation()} onChange={e => setLayout(e.target.value)} value={layout}>
+          <option value="cartesian">cartesian</option>
+          <option value="polar">polar</option>
+        </select>
+        {linkType === 'step' && layout !== 'polar' && (
+          <>
+            &nbsp;&nbsp;
+            <input
+              onClick={e => e.stopPropagation()}
+              type="range"
+              min={0}
+              max={1}
+              step={0.1}
+              onChange={e => setStepPercent(Number(e.target.value))}
+              value={stepPercent}
+              disabled={linkType !== 'step' || layout === 'polar'}
+            />
+          </>
+        )}
+      </div>
+      <div className={resetButton} onClick={resetColor}>
+        <ResetIcon />
+      </div>
     </div>
   );
 };
