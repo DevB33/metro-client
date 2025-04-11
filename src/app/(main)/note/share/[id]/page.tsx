b1d56372e9494732f68db42bb2876611605899cd
kiuuon <kiuuon29@gmail.com>
2025-04-11 16:56:53 +0900

    feat: 공유된 페이지 헤더 UI

---
+++ src/app/(main)/note/share/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { css } from '@/../styled-system/css';
+
+import axios from 'axios';
+import { cookies } from 'next/headers';
+
+import Header from './_components/header';
+
+const container = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: '100%',
+});
+
+const SharedNote = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const cookie = await cookies();
+  const accessToken = cookie?.get('accessToken')?.value;
+  const { id } = await params;
+
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/documents/${id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const noteData = response.data;
+
+  return (
+    <div className={container}>
+      <Header noteData={noteData} />
+      <div>contents</div>
+    </div>
+  );
+};
+
+export default SharedNote;
