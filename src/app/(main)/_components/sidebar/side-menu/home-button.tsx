15781a54f415c5cbc66a8199c856a446f64b0f7a
leedongyull <75784807+leedongyull@users.noreply.github.com>
2025-04-14 10:31:43 +0900

    Feat : homePage chart 구현 (#84)
    * feat : home 버튼 커서 pointer로 변경 및 이동 구현
    
    * feat : home 페이지 기본 틀 구현
    
    * feat : 기본 chart 띄우기 구현
    
    * fix : 잘못된 색상 컬러 타입 수정
    
    * feat : Link Type을 Step으로 고정
    
    * feat : node 의 크기를 줄이고 node간의 거리 조절
    
    * feat : 색상 reset 버튼 구현
    
    * feat : note API 연동
    
    * feat : 기본 tree 제거
    
    * feat : stepPercent 고정
    
    * feat : zoom 기능 추가
    
    * feat : 페이지 이동 및 페이지의 wheel과 충돌하지 않도록 zoom 조정
    
    * feat : 여러 오류 수정
    
    * feat : 불필요한 파일 삭제
    
    * feat : 불필요한 import 제거
    
    * feat : 로컬 스토리지로 layout 형태 저장

--- src/app/(main)/_components/sidebar/side-menu/home-button.tsx
+++ src/app/(main)/_components/sidebar/side-menu/home-button.tsx
@@ -1,6 +1,7 @@
 import { css } from '@/../styled-system/css';
 
 import HomeIcon from '@/icons/home-icon';
+import { useRouter } from 'next/navigation';
 
 const menuButton = css({
   height: '2rem',
@@ -14,11 +15,18 @@ const menuButton = css({
   borderRadius: '5px',
   backgroundColor: { base: 'none', _hover: '#F1F1F0' },
   transition: '0.2s',
+  cursor: 'pointer',
 });
 
 const HomeButton = () => {
+  const router = useRouter();
+
+  const goToHomepage = () => {
+    router.push(`/`);
+  };
+
   return (
-    <div className={menuButton}>
+    <div className={menuButton} onClick={goToHomepage}>
       <HomeIcon color="black" />
       Home
     </div>
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,32 @@
+import { css } from '@/../styled-system/css';
+import Example from './_components/chart/chart';
+
+const homePage = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100vw',
+  height: '100vh',
+  gap: 'base',
+});
+
+const welcomeBanner = css({
+  fontSize: 'lg',
+});
+
+const visualContainer = css({
+  width: '80%',
+  height: '60%',
+});
+
 const Home = () => {
   return (
-    <div>
-      <div>메인 페이지</div>
+    <div className={homePage}>
+      <div className={welcomeBanner}>김기원 님, 안녕하세요.</div>
+      <div className={visualContainer}>
+        <Example width={1300} height={600} />
+      </div>
     </div>
   );
 };
