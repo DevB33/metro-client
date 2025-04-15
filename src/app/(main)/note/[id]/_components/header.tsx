17cac0f383e2aa646a37e7ea82e04e3ee95644f3
이동율 <dy9623@naver.com>
2025-04-15 09:23:48 +0900

    feat : noteHeader 페이지 삭제 구현

--- src/app/(main)/note/[id]/_components/header.tsx
+++ src/app/(main)/note/[id]/_components/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter, useParams } from 'next/navigation';
+import { useRouter, useParams, usePathname } from 'next/navigation';
 import { css } from '@/../styled-system/css';
 
 import LeftArrowIcon from '@/icons/left-arrow-icon';
@@ -9,6 +9,8 @@ import RightArrowIcon from '@/icons/right-arrow-icon';
 import HorizonDotIcon from '@/icons/horizon-dot-icon';
 import DropDown from '@/components/dropdown/dropdown';
 import TrashIcon from '@/icons/trash-icon';
+import useSWR, { mutate } from 'swr';
+import { deletePage, getPageList } from '@/apis/side-bar';
 
 const headerConatiner = css({
   boxSizing: 'border-box',
@@ -44,6 +46,11 @@ const Header = () => {
   const params = useParams();
   const noteId = params.id as string;
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const pathname = usePathname();
+  const pageId = pathname.split('note/')[1];
+  console.log(pageId);
+
+  const { data: pageList } = useSWR('pageList');
 
   const openSettingDropdown = () => {
     setIsDropdownOpen(true);
@@ -55,6 +62,16 @@ const Header = () => {
 
   const sharePage = () => {
     router.push(`/note/share/${noteId}`);
+  };
+
+  const handleDeleteButtonClick = async () => {
+    try {
+      await deletePage(pageId);
+      await mutate('pageList', getPageList, false);
+    } catch (error) {
+      console.log(error);
+    }
+    closeSettingDropdown();
   };
 
   return (
@@ -71,7 +88,7 @@ const Header = () => {
           <HorizonDotIcon />
           <DropDown handleClose={closeSettingDropdown}>
             <DropDown.Menu isOpen={isDropdownOpen} top="0.5rem" right="0">
-              <DropDown.Item>
+              <DropDown.Item onClick={handleDeleteButtonClick}>
                 <TrashIcon />
                 삭제하기
               </DropDown.Item>
