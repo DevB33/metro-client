3aa59d96e3dce34aeb519fc56e06af5dcb08527a
이동율 <dy9623@naver.com>
2025-07-16 15:28:31 +0900

    fix : api가 수정되어 바뀐 부분 수정

--- src/app/(main)/_components/sidebar/finder/edit-title-modal.tsx
+++ src/app/(main)/_components/sidebar/finder/edit-title-modal.tsx
@@ -1,14 +1,15 @@
 import { css } from '@/../styled-system/css';
-import { getNoteList, editNoteIcon, editNoteTitle, getNoteInfo } from '@/apis/note';
-import IconSelector from '@/app/(main)/note/[id]/_components/note-header/icon-selector';
-import keyName from '@/constants/key-name';
-import SWR_KEYS from '@/constants/swr-keys';
-import { useClickOutside } from '@/hooks/useClickOutside';
-import PageIcon from '@/icons/page-icon';
-import INote from '@/types/note-type';
 import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import useSWR, { mutate } from 'swr';
+
+import { getNoteList, editNoteIcon, editNoteTitle, getNoteInfo } from '@/apis/note';
+import { useClickOutside } from '@/hooks/useClickOutside';
+import INote from '@/types/note-type';
+import keyName from '@/constants/key-name';
+import SWR_KEYS from '@/constants/swr-keys';
+import PageIcon from '@/icons/page-icon';
+import IconSelector from '@/app/(main)/note/[id]/_components/note-header/icon-selector';
 
 interface IEditTitleModal {
   noteId: string;
@@ -19,98 +20,47 @@ interface IEditTitleModal {
   bottom?: number;
 }
 
-const container = css({
-  position: 'absolute',
-  display: 'flex',
-  flexDirection: 'column',
-});
-
-const modalContainer = css({
-  position: 'absolute',
-  width: '23.75rem',
-  height: '2rem',
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  backgroundColor: 'white',
-  boxShadow: 'dropDown',
-  zIndex: '10000',
-  borderRadius: 'lg',
-  gap: 'tiny',
-  px: '0.5rem',
-});
-
-const iconContainer = css({
-  width: '1.8rem',
-  height: '1.8rem',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  fontSize: '1rem',
-  borderRadius: 'sm',
-  borderWidth: '1px',
-  borderColor: 'gray',
-  cursor: 'pointer',
-});
-
-const inputContainer = css({
-  flex: '1',
-  height: '1.6rem',
-  backgroundColor: 'lightGray',
-  borderRadius: 'sm',
-  borderWidth: '1px',
-  borderColor: 'gray',
-  px: '0.2rem',
-  outline: 'none',
-});
-
-const IconSelectorContainer = css({
-  position: 'absolute',
-  top: '2.5rem',
-  zIndex: '10001',
-});
-
 const EditTitleModal = ({ noteId, closeEditModal, top, left, right, bottom }: IEditTitleModal) => {
   const { data } = useSWR(SWR_KEYS.NOTE_LIST);
 
   const [value, setValue] = useState('');
   const [isSelectorOpen, setIsSelectorOpen] = useState(false);
-  const debounceTimer = useRef<NodeJS.Timeout | null>(null);
   const [noteNode, setNoteNode] = useState<INote | null>(null);
 
+  const debounceTimer = useRef<NodeJS.Timeout | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const editModalRef = useClickOutside(closeEditModal);
 
-  const findNode = (nodes: INote[], id: string): INote | null => {
-    if (!nodes) return null;
+  const findNote = (notes: INote[], id: string): INote | null => {
+    if (!notes) return null;
 
-    const directFoundNode = nodes.find(node => node.id === id);
-    if (directFoundNode) return directFoundNode;
+    const directFoundNote = notes.find(node => node.id === id);
+    if (directFoundNote) return directFoundNote;
 
-    let foundNode: INote | null = null;
-    nodes.some(node => {
-      if (node.children?.length) {
-        foundNode = findNode(node.children, id);
+    let foundNote: INote | null = null;
+    notes.some(note => {
+      if (note.children?.length) {
+        foundNote = findNote(note.children, id);
       }
-      return foundNode !== null;
+      return foundNote !== null;
     });
 
-    return foundNode;
+    return foundNote;
   };
 
   useEffect(() => {
-    const foundNode = findNode(data.node, noteId);
-    setNoteNode(foundNode);
+    const foundNote = findNote(data, noteId);
+    setNoteNode(foundNote);
   }, [data]);
 
   useEffect(() => {
     if (!data) return;
 
-    const foundNode = findNode(data.node, noteId);
-    setNoteNode(foundNode);
+    const foundNote = findNote(data, noteId);
+    setNoteNode(foundNote);
 
-    if (foundNode) {
-      setValue(foundNode.title ?? '');
+    if (foundNote) {
+      setValue(foundNote.title ?? '');
     }
 
     setTimeout(() => {
@@ -165,9 +115,9 @@ const EditTitleModal = ({ noteId, closeEditModal, top, left, right, bottom }: IE
   return ReactDOM.createPortal(
     <div ref={editModalRef} className={container} style={{ top, left, right, bottom }}>
       <div className={modalContainer}>
-        <div className={iconContainer} onClick={handleSelectorOpen}>
+        <button type="button" className={iconContainer} onClick={handleSelectorOpen}>
           {noteNode?.icon ?? <PageIcon color="#949491" />}
-        </div>
+        </button>
         <input
           ref={inputRef}
           value={value || ''}
@@ -187,5 +137,56 @@ const EditTitleModal = ({ noteId, closeEditModal, top, left, right, bottom }: IE
     document.body,
   );
 };
+
+const container = css({
+  position: 'absolute',
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const modalContainer = css({
+  position: 'absolute',
+  width: '23.75rem',
+  height: '2rem',
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  backgroundColor: 'white',
+  boxShadow: 'dropDown',
+  zIndex: '10000',
+  borderRadius: 'lg',
+  gap: 'tiny',
+  px: '0.5rem',
+});
+
+const iconContainer = css({
+  width: '1.8rem',
+  height: '1.8rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '1rem',
+  borderRadius: 'sm',
+  borderWidth: '1px',
+  borderColor: 'gray',
+  cursor: 'pointer',
+});
+
+const inputContainer = css({
+  flex: '1',
+  height: '1.6rem',
+  backgroundColor: 'lightGray',
+  borderRadius: 'sm',
+  borderWidth: '1px',
+  borderColor: 'gray',
+  px: '0.2rem',
+  outline: 'none',
+});
+
+const IconSelectorContainer = css({
+  position: 'absolute',
+  top: '2.5rem',
+  zIndex: '10001',
+});
 
 export default EditTitleModal;
